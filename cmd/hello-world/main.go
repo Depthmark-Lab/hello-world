0dba44f727ddcdacf9daa352113ab2f88c172854
Alexandre Delisle <alexodelisle@gmail.com>
2026-03-29 17:24:18 -0400

    feat: new feature

--- cmd/hello-world/main.go
+++ cmd/hello-world/main.go
@@ -6,7 +6,7 @@ func Greet(name string) string {
 	if name == "" {
 		return "Hello, World!"
 	}
-	return fmt.Sprintf("Hello Hello hello, %s!", name)
+	return fmt.Sprintf("Hello v2, %s!", name)
 }
 
 func main() {
